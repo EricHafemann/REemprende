00968e6360bf908b3cb3481fe0d9d275a8687ad4
MuriloHJ <murilo_joly@estudante.sesisenai.org.br>
2026-03-06 15:56:59 -0300

    Feat(model): Adiciona os costrutores e os getters e setters na classe Usuario

--- src/main/java/org/example/model/Usuario.java
+++ src/main/java/org/example/model/Usuario.java
@@ -1,4 +1,65 @@
 package org.example.model;
 
-public class Usuario {
+public class Usuario
+{
+    private long id;
+    private String email;
+    private String senha;
+    private String nome;
+
+
+    public Usuario(long id, String email, String senha, String nome)
+    {
+        this.id = id;
+        this.email = email;
+        this.senha = senha;
+        this.nome = nome;
+    }
+
+    public Usuario(String email, String senha, String nome)
+    {
+        this.email = email;
+        this.senha = senha;
+        this.nome = nome;
+    }
+
+    public long getId()
+    {
+        return id;
+    }
+
+    public void setId(long id)
+    {
+        this.id = id;
+    }
+
+    public String getEmail()
+    {
+        return email;
+    }
+
+    public void setEmail(String email)
+    {
+        this.email = email;
+    }
+
+    public String getSenha()
+    {
+        return senha;
+    }
+
+    public void setSenha(String senha)
+    {
+        this.senha = senha;
+    }
+
+    public String getNome()
+    {
+        return nome;
+    }
+
+    public void setNome(String nome)
+    {
+        this.nome = nome;
+    }
 }
