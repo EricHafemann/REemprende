16919646f0a79b92eff240a68e7177b126d55ac2
ENZO VENTURI <enzo_venturi@estudante.sesisenai.org.br>
2026-03-06 20:19:46 -0300

    Feat(view, utility): Adiciona métodos.
    - sair (view)

---
+++ src/main/java/org/example/presenter/InicialPresenter.java
@@ -0,0 +1,10 @@
+package org.example.presenter;
+
+public class InicialPresenter {
+    public void trocarDeView(int opcao) {
+        switch (opcao) {
+            case 1 ->
+            case 2 ->
+        }
+    }
+}
---
+++ src/main/java/org/example/view/interfaces/InterfaceView.java
@@ -0,0 +1,7 @@
+package org.example.view.interfaces;
+
+public interface InterfaceView {
+    void exibirErro(String mensagem);
+    void exibirSucesso(String mensagem);
+    void sair(String mensagem);
+}
