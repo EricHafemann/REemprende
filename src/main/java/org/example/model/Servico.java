754d42c7e8e59949ddd775efa1dac87d0912df45
MuriloHJ <murilo_joly@estudante.sesisenai.org.br>
2026-03-06 16:15:38 -0300

    Feat(model): Adiciona os contrutores getters e setters na classe Servico

--- src/main/java/org/example/model/Servico.java
+++ src/main/java/org/example/model/Servico.java
@@ -1,4 +1,64 @@
 package org.example.model;
 
-public class Servico {
+public class Servico
+{
+    private long idServico;
+    private String avalicao;
+    private String descricao;
+    private double duracaoHoras;
+
+    public Servico(long idServico, String avalicao, String descricao, double duracaoHoras)
+    {
+        this.idServico = idServico;
+        this.avalicao = avalicao;
+        this.descricao = descricao;
+        this.duracaoHoras = duracaoHoras;
+    }
+
+    public Servico(String avalicao, String descricao, double duracaoHoras)
+    {
+        this.avalicao = avalicao;
+        this.descricao = descricao;
+        this.duracaoHoras = duracaoHoras;
+    }
+
+    public long getIdServico()
+    {
+        return idServico;
+    }
+
+    public void setIdServico(long idServico)
+    {
+        this.idServico = idServico;
+    }
+
+    public String getAvalicao()
+    {
+        return avalicao;
+    }
+
+    public void setAvalicao(String avalicao)
+    {
+        this.avalicao = avalicao;
+    }
+
+    public String getDescricao()
+    {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao)
+    {
+        this.descricao = descricao;
+    }
+
+    public double getDuracaoHoras()
+    {
+        return duracaoHoras;
+    }
+
+    public void setDuracaoHoras(double duracaoHoras)
+    {
+        this.duracaoHoras = duracaoHoras;
+    }
 }
