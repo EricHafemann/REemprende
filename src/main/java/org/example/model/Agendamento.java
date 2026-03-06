ff4765859dd0c8e1914bb4cc2cefbff2788d8c62
GABRIEL BEHLING <gabriel_behling@estudante.sesisenai.org.br>
2026-03-06 15:57:41 -0300

    feat(model): adiciona construtor, atributos e métodos em agendamento.

--- src/main/java/org/example/model/Agendamento.java
+++ src/main/java/org/example/model/Agendamento.java
@@ -1,4 +1,51 @@
 package org.example.model;
 
+import java.time.LocalDate;
+
 public class Agendamento {
+
+    private long idAgendamento;
+    private LocalDate dataInicio;
+    private LocalDate dataFim;
+    private String Observacao;
+
+    public Agendamento(long idAgendamento, LocalDate dataInicio,
+                       LocalDate dataFim, String observacao) {
+        this.idAgendamento = idAgendamento;
+        this.dataInicio = dataInicio;
+        this.dataFim = dataFim;
+        Observacao = observacao;
+    }
+
+    public long getIdAgendamento() {
+        return idAgendamento;
+    }
+
+    public void setIdAgendamento(long idAgendamento) {
+        this.idAgendamento = idAgendamento;
+    }
+
+    public LocalDate getDataInicio() {
+        return dataInicio;
+    }
+
+    public void setDataInicio(LocalDate dataInicio) {
+        this.dataInicio = dataInicio;
+    }
+
+    public LocalDate getDataFim() {
+        return dataFim;
+    }
+
+    public void setDataFim(LocalDate dataFim) {
+        this.dataFim = dataFim;
+    }
+
+    public String getObservacao() {
+        return Observacao;
+    }
+
+    public void setObservacao(String observacao) {
+        Observacao = observacao;
+    }
 }
