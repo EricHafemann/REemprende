a04275e9a98e8217bb67206d3ee4b1a0ddf67d20
GABRIEL BEHLING <gabriel_behling@estudante.sesisenai.org.br>
2026-03-06 16:24:29 -0300

    feat(view): adiciona view da tela inicial, inlcuindo método.

---
+++ src/main/java/org/example/view/InicialView.java
@@ -0,0 +1,31 @@
+package org.example.view;
+
+public class InicialView {
+    public static void mostraTelaInicial()
+    {
+        String RESET = "\u001B[0m";
+        String CIANO = "\u001B[36m";
+        String VERDE = "\u001B[32m";
+        String AMARELO = "\u001B[33m";
+        String NEGRITO = "\u001B[1m";
+
+        System.out.println(CIANO + "===============================================" + RESET);
+        System.out.println(CIANO + "||                                           ||" + RESET);
+        System.out.println(CIANO + "||   " + NEGRITO + AMARELO + "Bem-vindo ao REemprende" + RESET + CIANO + "    ||" + RESET);
+        System.out.println(CIANO + "||                                           ||" + RESET);
+        System.out.println(CIANO + "===============================================" + RESET);
+
+        System.out.println("\n" + VERDE + "  [✓] Conexão estabelecida com sucesso." + RESET);
+
+        System.out.println("\n" + AMARELO + "-----------------------------------------------" + RESET);
+        System.out.println("  Iniciado em: " + new java.util.Date(5000));
+        System.out.println(AMARELO + "-----------------------------------------------" + RESET);
+
+        System.out.println("\n" + NEGRITO + "Digite a opção desejada!" + RESET);
+        System.out.println("\n" + NEGRITO + "1 = Cadastro:" + RESET);
+        System.out.println("\n" + NEGRITO + "2 = Login" + RESET);
+        System.out.println("\n" + NEGRITO + "3 = Sair" + RESET);
+        System.out.println("\n" + NEGRITO + "Resposta: " + RESET);
+        System.out.print("> ");
+    }
+}
