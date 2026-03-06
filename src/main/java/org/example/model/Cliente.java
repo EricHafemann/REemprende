43c532fa75c1e5a2f8b0bc2ff11e81a3c2341e8a
EricHafemann <eric070gabriel@gmail.com>
2026-03-06 16:09:54 -0300

    feat(model): adiciona atributos e métodos da classe Cliente.

--- src/main/java/org/example/model/Cliente.java
+++ src/main/java/org/example/model/Cliente.java
@@ -1,4 +1,24 @@
 package org.example.model;
 
-public class Cliente {
+public class Cliente extends Usuario{
+
+    private String cpf;
+
+    public Cliente(long id, String email, String senha, String nome, String cpf) {
+        super(id, email, senha, nome);
+        this.cpf = cpf;
+    }
+
+    public Cliente(String email, String senha, String nome, String cpf) {
+        super(email, senha, nome);
+        this.cpf = cpf;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
 }
