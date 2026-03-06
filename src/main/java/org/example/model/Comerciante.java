1f2f3f0c7bb10c2e2bfe29766ba39c6114602eca
ENZO VENTURI <enzo_venturi@estudante.sesisenai.org.br>
2026-03-06 16:14:12 -0300

    Feat(model): Implementa Construtor e Getters e Setters na Classe Comerciante

--- src/main/java/org/example/model/Comerciante.java
+++ src/main/java/org/example/model/Comerciante.java
@@ -1,4 +1,27 @@
 package org.example.model;
 
 public class Comerciante {
+    private String cnpj;
+    private String senhaAcesso;
+
+    public Comerciante(String cnpj, String senhaAcesso) {
+        this.cnpj = cnpj;
+        this.senhaAcesso = senhaAcesso;
+    }
+
+    public String getCnpj() {
+        return cnpj;
+    }
+
+    public void setCnpj(String cnpj) {
+        this.cnpj = cnpj;
+    }
+
+    public String getSenhaAcesso() {
+        return senhaAcesso;
+    }
+
+    public void setSenhaAcesso(String senhaAcesso) {
+        this.senhaAcesso = senhaAcesso;
+    }
 }
